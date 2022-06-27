a9dfe99b1c6b842953afc1ef8538a87f88cd6c81
Ángel Barruz <a.barruz@gmail.com>
2022-06-27 21:37:06 +0200

    en proceso

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
 #imports
+import requests
+import pandas as pd
+import re
+import json
+import math
 
+API_TOKEN = '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'
+USERNAME = 'angel-barruz'
+BASE_URL = 'https://api.github.com/'
+KEY = 'repos/'
+OWNER = 'ih-datapt-mad/'
+REPO =  'dataptmad0522_labs/'
+SEARCH = 'search/issues?q=repo:'+OWNER+REPO+'+type:pr+state:{}'
+PULLS = 'pulls?page={}&per_page=100&state={}'
+COMMITS = 'pulls/{}/commits'
+STATE = 'open'
 
+field_list1 = ['number',
+               'title',
+               'state',
+               'created_at',
+               'updated_at',
+               'closed_at',
+               'html_url',
+               'base.repo.full_name',
+               'base.ref',
+               'head.repo.full_name',
+               'head.ref',
+               'head.repo.pushed_at']
+
+ field_list2 = ['student_name',
+               'number',
+               'lab_name',
+               'state',
+               'lab_status',
+               'created_at',
+               'updated_at',
+               'closed_at',
+               'html_url',
+               'base.repo.full_name',
+               'base.ref',
+               'head.repo.full_name',
+               'head.ref',
+               'head.repo.pushed_at']
+
+field_sort1 = ['lab_status',
+               'lab_name',
+               'student_name']
+               
 def main():
   
 
